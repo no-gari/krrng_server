d2e605ed94aff2c15525ef5924c373792317f4c3
no-gari <nojonghyeok@nojonghyeog-ui-MacBookAir.local>
2022-12-05 23:31:05 +0900

    add point noti

--- api/point/signals.py
+++ api/point/signals.py
@@ -1,5 +1,5 @@
-import requests
 from .models import PointLog
+from api.customerservice.models import Notification
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -9,27 +9,12 @@ def pointlog_post_save(sender, **kwargs):
     user = kwargs['instance'].user
     title = kwargs['instance'].title
     content = kwargs['instance'].reason
+    point = kwargs['instance'].amount
 
-    url = "https://onesignal.com/api/v1/notifications"
-
-    payload = {
-        "include_external_user_ids": [
-            str(user.id)
-        ],
-        "app_id": "c028e613-8406-43a8-ba01-fbff5754aa95",
-        "headings": {
-            "ko": title,
-            "en": title,
-        },
-        "contents": {
-            "ko": content,
-            "en": content,
-        },
-        "name": "INTERNAL_CAMPAIGN_NAME"
-    }
-    headers = {
-        "accept": "application/json",
-        "Authorization": 'Basic MzM5OTk3MjAtOTNkYi00ODRlLWE2YjctNDE0MDYzN2FmYzk5',
-        "Content-Type": "application/json"
-    }
-    requests.post(url, json=payload, headers=headers)
+    noti = Notification.objects.create(
+        sort='적립' if point >= 0 else '차감',
+        user=user,
+        title=title,
+        content=content
+    )
+    noti.save()
