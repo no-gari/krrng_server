ef3fd3ac0e001e504a151394e9f30709df9997ff
no-gari <nojonghyeok@nojonghyeog-ui-MacBookAir.local>
2022-12-11 15:41:02 +0900

    change symptom and disease

--- api/disease/models.py
+++ api/disease/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 
 
-class Symptom(models.Model):
-    name = models.CharField(max_length=128, verbose_name='증상명')
+class Disease(models.Model):
+    name = models.CharField(max_length=128, verbose_name='정확한 질병명')
 
     class Meta:
-        verbose_name = '증상 명'
+        verbose_name = '질병 명'
         verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.name
 
 
-class Disease(models.Model):
-    symptom = models.ManyToManyField(Symptom, verbose_name='증상')
-    name = models.CharField(max_length=128, verbose_name='정확한 질병명')
+
+class Symptom(models.Model):
+    disease = models.ManyToManyField(Disease, verbose_name='질병')
+    name = models.CharField(max_length=128, verbose_name='증상명')
 
     class Meta:
-        verbose_name = '질병 명'
+        verbose_name = '증상 명'
         verbose_name_plural = verbose_name
 
     def __str__(self):
