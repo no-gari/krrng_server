3f1fde8fb379198dcb84d6cd9323c9d0f0175391
no-gari <nojonghyeok@nojonghyeog-ui-MacBookAir.local>
2023-01-04 10:11:24 +0900

    fix token

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,11 +5,6 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 pymysql.install_as_MySQLdb()
 
-
-class SecurityTokenWorkaroundS3Boto3Storage(S3Boto3Storage):
-    def _get_security_token(self):
-        return None
-
 DEBUG = True
 
 CORS_ALLOW_ALL_ORIGINS = True
