fedaac7c1a9a61cf6c8ab1ea481d9358b41313b1
no-gari <nojonghyeok@nojonghyeog-ui-MacBookAir.local>
2023-01-08 04:01:08 +0900

    fix animal create view

--- api/animal/serializers.py
+++ api/animal/serializers.py
@@ -18,20 +18,8 @@ class AnimalCreateSerializer(serializers.ModelSerializer):
     def get_user(self, obj):
         return self.context['request'].user.id
 
-    def save(self):
-        user = self.context['request'].user
-        name = self.validated_data.get('name'),
-        sort = self.validated_data.get('sort'),
-        birthday = self.validated_data.get('birthday'),
-        weight = self.validated_data.get('weight'),
-        kind = self.validated_data.get('kind'),
-        hospital_address = self.validated_data.get('hospital_address'),
-        hospital_address_detail = self.validated_data.get('hospital_address_detail'),
-        interested_disease = self.validated_data.get('interested_disease'),
-        neuter_choices = self.validated_data.get('neuter_choices'),
-        has_alergy = self.validated_data.get('has_alergy'),
-        sex_choices = self.validated_data.get('sex_choices'),
-        image = self.validated_data.get('image')
+    def create(self, attrs):
+        return attrs
 
 
 class SortAnimalSerializer(serializers.ModelSerializer):
--- api/animal/views.py
+++ api/animal/views.py
@@ -56,9 +56,30 @@ class AnimalCreateAPIView(CreateAPIView):
     allowed_methods = ['GET', 'POST']
     permission_classes = [IsAuthenticated]
 
+    def create(self, request, *args, **kwargs):
+        try:
+            new_animal = Animal.objects.create(
+                user=request.user,
+                name=request.data.get('name'),
+                sort=request.data.get('sort'),
+                birthday=request.data.get('birthday'),
+                weight=request.data.get('weight'),
+                kind=request.data.get('kind'),
+                hospital_address=request.data.get('hospital_address'),
+                hospital_address_detail=request.data.get('hospital_address_detail'),
+                interested_disease=request.data.get('interested_disease'),
+                neuter_choices=request.data.get('neuter_choices'),
+                has_alergy=request.data.get('has_alergy'),
+                sex_choices=request.data.get('sex_choices'),
+                image=request.FILES.get('image')
+            )
+            new_animal.save()
+        except:
+            return ValidationError({'error_msg': '다시 한 번 시도 해주세요.'})
+        return super().create(request, *args, **kwargs)
 
 @api_view(["DELETE"])
-def delete_Animal(request, *args, **kwargs):
+def delete_animal(request, *args, **kwargs):
     if not request.user.is_authenticated:
         return Response({'error_msg': '로그인 후 이용해주세요,'}, status=status.HTTP_400_BAD_REQUEST)
     try:
