7d6e0135899c1c669087e32e4bbc0bda36b31f19
no-gari <sofaissofa@icloud.com>
2023-07-31 14:49:37 +0900

    remove credential for production setting

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,17 +6,6 @@ CORS_ALLOWED_ORIGINS = [
     'https://frontend-domain.com',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres123!',
-        'HOST': 'postgres',
-        'PORT': '5432',
-    }
-}
-
 # S3
 AWS_ACCESS_KEY_ID = '**'
 AWS_SECRET_ACCESS_KEY = '**'
